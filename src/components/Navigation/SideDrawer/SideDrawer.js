849a9b477df27b1edb150b4a44d8c798d35ab76c
devrhathi <devrhathi@gmail.com>
2020-11-10 10:54:13 +0530

    first commit

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxillary from '../../../hoc/Auxillary/Auxillary';
+
+const sideDrawer = (props)=>{ 
+    let attachClasses = [classes.SideDrawer, classes.Close];
+    if (props.open){
+        attachClasses = [classes.SideDrawer, classes.Open];
+    }
+
+    return(
+        <Auxillary>
+        <Backdrop show={props.open} clicked={props.closed}/>
+        <div className={attachClasses.join(' ')}>
+                <div className={classes.Logo}>
+            <Logo/>
+            </div>
+            <nav>
+                <NavigationItems/>
+            </nav>
+        </div>
+        </Auxillary>
+    );
+};
+
+
+export default sideDrawer;
